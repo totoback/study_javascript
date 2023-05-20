ce014b7454a51be37259fdd8059b586ab161f51b
지영 <toto.joung@gmail.com>
2023-05-20 21:42:50 +0900

    230520

--- todoList/todo.js
+++ todoList/todo.js
@@ -1,29 +1,35 @@
+const saveBtn = document.querySelector(".main-btn");
+const inputEl = document.querySelector(".todo-input input");
+const ulEl = document.querySelector(".todo-contents");
 
+function todoList() {
+  const liEl = document.createElement("li");
+  const liText = document.createElement("span");
+  const inputCheck = document.createElement("input");
+  const deleteBtn = document.createElement("button");
 
-
-
-
-
-const saveBtn = document.querySelector(".main-btn")
-const inputEl = document.querySelector(".todo-input input")
-const ulEl = document.querySelector(".todo-contents")
-const liEl = document.createElement("li")
-const spanEl = document.createElement("span")
-
-function todoList(){
   //텍스트 입력창 띄우기
-  if(inputEl.value === ""){
-    inputEl.setAttribute("placeholder","텍스트를 입력해주세요.")
+  if (inputEl.value === "") {
+    inputEl.setAttribute("placeholder", "텍스트를 입력해주세요.");
     return;
   }
   // 입력값 받기
-  const inputText = inputEl.value; //입력 값
-  liEl.innerText = inputText;
-  console.log(inputText)
-  
-  ulEl.append(liEl)
+  liText.textContent = inputEl.value;
+  inputCheck.setAttribute("type", "checkbox");
+  deleteBtn.innerText = "삭제";
+
+  //삭제 버튼 구현
+  deleteBtn.addEventListener("click", function (e) {
+    if(inputCheck.checked === true){
+      e.target.parentNode.remove();
+    }
+  });
+
+  ulEl.appendChild(liEl); //ul 안에 li 추가
+  liEl.append(inputCheck); //li 안에 체크박스 추가
+  liEl.append(liText); // li안에 span추가
+  liEl.append(deleteBtn); // li안에 삭제버튼 추가
   inputEl.value = "";
 }
 
-saveBtn.addEventListener("click",todoList);
-
+saveBtn.addEventListener("click", todoList);
