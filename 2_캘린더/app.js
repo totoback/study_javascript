db15c8e0eeac3e2bfec8711b91abe2ac55521bfc
지영 <toto.joung@gmail.com>
2023-05-21 18:45:02 +0900

    230521_캘린더 완성

--- 2_캘린더/app.js
+++ 2_캘린더/app.js
@@ -1,30 +1,34 @@
 const inputDate = document.querySelector("#date");
 const contentData = document.querySelector("#content");
 const btn = document.querySelector("button");
+
 let targetEl;
 
 function calendarTask(e) {
-  console.log(e.target);
-
   if (e.target.tagName === "P") {
     targetEl = e.target.parentNode;
     inputDate.value = `오늘은 ${e.target.textContent}일`;
   }
-  if (e.targettagNage === "TD") {
-    targetEl = e.target;
-  }
 
   btn.addEventListener("click", function (e) {
-    //예외 처리
-    if (inputDate.value == "" && contentData.value == "") {
-      alert("날짜를와 내용을 입력해주세요");
+    // 예외 처리
+    if (inputDate.value === "" && contentData.value === "") {
+      alert("날짜와 내용을 입력해주세요.");
+      return;
+    } else if (inputDate.value === "") {
+      alert("날짜를 선택해주세요");
+      return;
+    } else if (contentData.value === "") {
+      alert("내용을 입력해주세요.");
+      return;
     }
-    targetEl = e.target;
-    const createP = document.createElement("p");
-    createP.textContent = inputDate.value;
+    const addDiv = document.createElement("div");
+    addDiv.textContent = contentData.value;
+    
+    targetEl.append(addDiv);
+    inputDate.value = "";
+    contentData.value = "";
   });
-  targetEl.append(createP);
-  contentData.value = "";
 }
 
 addEventListener("click", calendarTask);
