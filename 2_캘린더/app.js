1defd65a9bb7b172c3e14930ebd12e57c139af1d
지영 <toto.joung@gmail.com>
2023-05-21 18:46:55 +0900

    230521_코드위치수정

--- 2_캘린더/app.js
+++ 2_캘린더/app.js
@@ -9,26 +9,26 @@ function calendarTask(e) {
     targetEl = e.target.parentNode;
     inputDate.value = `오늘은 ${e.target.textContent}일`;
   }
-
-  btn.addEventListener("click", function (e) {
-    // 예외 처리
-    if (inputDate.value === "" && contentData.value === "") {
-      alert("날짜와 내용을 입력해주세요.");
-      return;
-    } else if (inputDate.value === "") {
-      alert("날짜를 선택해주세요");
-      return;
-    } else if (contentData.value === "") {
-      alert("내용을 입력해주세요.");
-      return;
-    }
-    const addDiv = document.createElement("div");
-    addDiv.textContent = contentData.value;
-    
-    targetEl.append(addDiv);
-    inputDate.value = "";
-    contentData.value = "";
-  });
 }
+
+btn.addEventListener("click", function (e) {
+  // 예외 처리
+  if (inputDate.value === "" && contentData.value === "") {
+    alert("날짜와 내용을 입력해주세요.");
+    return;
+  } else if (inputDate.value === "") {
+    alert("날짜를 선택해주세요");
+    return;
+  } else if (contentData.value === "") {
+    alert("내용을 입력해주세요.");
+    return;
+  }
+  const addDiv = document.createElement("div");
+  addDiv.textContent = contentData.value;
+  
+  targetEl.append(addDiv);
+  inputDate.value = "";
+  contentData.value = "";
+});
 
 addEventListener("click", calendarTask);
