658ae788e10e898c2fce0b6e609f0c5cc327c8ca
지영 <toto.joung@gmail.com>
2023-05-30 19:55:40 +0900

    230530_탭버튼까지 완

--- 7_메뉴판/app.js
+++ 7_메뉴판/app.js
@@ -50,25 +50,48 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
+// load items
 window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function (item) {
-    // console.log(item)
-
-    return `<article class="menu-item">
-    <img src=${item.img} alt=${item.title} class="photo" />
-    <div class="item-info">
-      <header>
-        <h4>${item.title}</h4>
-        <h4 class="price">${item.price}</h4>
-      </header>
-      <p class="item-text">
-        ${item.desc}
-      </p>
-    </div>
-  </article>`;
-  });
-  
-  displayMenu = displayMenu.join("");
-  sectionCenter.innerHTML = displayMenu
+  displayMenuItems(menu);
 });
+
+// filter items
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.target.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+      if (category === "all") {
+        return true; // 모든 항목을 반환
+      } else if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+
+    displayMenuItems(menuCategory);
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+      <img src=${item.img} alt=${item.title} class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">${item.price}</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>`;
+  });
+
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
