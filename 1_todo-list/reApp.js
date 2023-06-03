f6f1fccb0271902da258320da64f1d572ad4a346
지영 <toto.joung@gmail.com>
2023-06-03 17:14:14 +0900

    230603_투두 다시

---
+++ 1_todo-list/reApp.js
@@ -0,0 +1,33 @@
+const inputEl = document.querySelector(".input");
+const btn = document.querySelector(".main-btn");
+const ulEl = document.querySelector(".todo-contents");
+
+function todoList() {
+  //빈 텍스트 예외 처리
+  if (inputEl.value === "") {
+    inputEl.setAttribute("placeholder", "내용을 입력해주세요");
+  }
+
+  const liEl = document.createElement("li");
+  const spanEl = document.createElement("span");
+  const checkbox = document.createElement("input");
+  const deleteBtn = document.createElement("button");
+
+  checkbox.setAttribute("type", "checkbox");
+  spanEl.textContent = inputEl.value;
+  deleteBtn.textContent = "삭제";
+
+  deleteBtn.addEventListener("click", function (e) {
+    if (checkbox.checked === true) {
+      e.target.parentNode.remove()
+    }
+  });
+
+  ulEl.appendChild(liEl); //ul 안에 li 추가
+  liEl.append(checkbox);
+  liEl.append(spanEl);
+  liEl.append(deleteBtn);
+  inputEl.value = "";
+}
+
+btn.addEventListener("click", todoList);
