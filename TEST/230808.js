8bcc93e820aa81e141c3af77f0774bb494dba200
지영 <toto.joung@gmail.com>
2023-08-08 16:49:04 +0900

    230808_모음제거

--- TEST/230808.js
+++ TEST/230808.js
@@ -15,12 +15,29 @@
 //   console.log(result);
 //   return result;
 // }
-function solution(n) {
-  const nums = n
-    .toString()
-    .split("")
-    .reduce((acc, curr) => acc + Number(curr), 0);
-  console.log(nums);
+// function solution(n) {
+//   const nums = n
+//     .toString()
+//     .split("")
+//     .reduce((acc, curr) => acc + Number(curr), 0);
+//   console.log(nums);
+// }
+
+// solution(1234);
+
+//**모음제거 */
+// 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
+// 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+function solution(my_string) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let result = [];
+
+  for(let i = 0; i < my_string.length; i++) {
+      if(!vowels.includes(my_string[i])) {
+          result.push(my_string[i]);
+      }
+  }
+  return result.join("");
 }
 
-solution(1234);
+solution("bus")
